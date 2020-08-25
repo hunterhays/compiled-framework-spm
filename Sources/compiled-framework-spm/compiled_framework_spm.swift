e87e7c6308888377b52cdbc0ba6a59ece73366a0
hunterhays <hhays@yahoo-inc.com>
2020-08-24 20:15:00 -0700

    Update compiled_framework_spm.swift

--- Sources/compiled-framework-spm/compiled_framework_spm.swift
+++ Sources/compiled-framework-spm/compiled_framework_spm.swift
@@ -1,16 +1,3 @@
 import Foundation
 import CryptoKit
 import FlurryMessaging
-
-let logger = Logger(label: "com.example.compiledFramework")
-
-func foo() {
-    logger.info("foo called")
-}
-
-func bar() -> SHA256Digest {
-    let string = "Hello, world!"
-    let inputData = Data(string.utf8)
-    
-    return SHA256.hash(data: inputData)
-}
