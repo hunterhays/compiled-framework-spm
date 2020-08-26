8608a556d96bf7eeb8a69b8798e0403867003800
npatel05 <nirha.patel@verizonmedia.com>
2020-08-26 16:27:52 -0700

    dependency

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "compiled-framework-spm",
-            dependencies: ["FlurryMessaging"]
+            dependencies: ["FlurryMessaging", "FlurryConfig", "Flurry"]
         ),
         .binaryTarget(
             name: "FlurryMessaging",
