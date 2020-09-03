c20638afdeffdd9a12f726636b019d899c565083
hunterhays <hhays@yahoo-inc.com>
2020-09-03 10:07:46 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Flurry",
+            name: "FlurrySPM",
             targets: ["Flurry"]
         )
     ],
@@ -44,7 +44,7 @@ let package = Package(
         ),
         
         .testTarget(
-            name: "Flurry",
+            name: "FlurrySPM",
             dependencies: ["Flurry"]
         ),
     ]
